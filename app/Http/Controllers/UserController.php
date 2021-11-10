2a8725baa6ab203ee7e30c70086da36142434222
Itsqy <syatriarifqi@gmail.com>
2021-11-10 14:41:26 +0700

    first test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth']);
     }
 
     public function index()
--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ use App\Http\Controllers\Landing\LandingController;
 
 
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/profile', UserController::class)->middleware('auth');
