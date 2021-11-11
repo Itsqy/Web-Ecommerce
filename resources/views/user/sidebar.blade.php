ac31a3145edd5fff3e752ce764a107d027ffc358
Itsqy <syatriarifqi@gmail.com>
2021-11-11 11:00:39 +0700

    first test

--- resources/views/user/sidebar.blade.php
+++ resources/views/user/sidebar.blade.php
@@ -4,11 +4,11 @@
          <div class="sidebar-content">
              <div class="user">
                  <div class="avatar-sm float-left mr-2">
-                     @if (Auth::user()->img == '')
+                     @if (Auth::user()->image == '')
                          <img src="https://ui-avatars.com/api/?name={{ Auth::user()->username }}" alt="..."
                              class="avatar-img rounded-circle">
                      @else
-                         <img src="{{ $User->img }}" class="avatar-img rounded-circle" alt="">
+                         <img src="{{ $User->image }}" class="avatar-img rounded-circle" alt="">
                      @endif
 
                  </div>
