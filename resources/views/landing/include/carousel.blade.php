9fad2cc20597fefc9a92c12114361dc18c5fbe52
Itsqy <syatriarifqi@gmail.com>
2021-11-11 12:53:52 +0700

    first test

--- resources/views/landing/include/carousel.blade.php
+++ resources/views/landing/include/carousel.blade.php
@@ -1,13 +1,14 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="{{asset('landing/img/w1.webp')}}" class="d-block w-100" alt="...">
+            <img src="{{ asset('landing/img/w1.webp') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{asset('landing/img/w3.jfif')}}" class="d-block w-100" alt="...">
+            <img src="{{ url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLTgnATltRq7VU2W6tnwAzzHbpb1bdNR2Ccg&usqp=CAU') }}"
+                class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{asset('landing/img/w2.jpg')}}" class="d-block w-100" alt="...">
+            <img src="{{ asset('landing/img/w2.jpg') }}" class="d-block w-100" alt="...">
         </div>
     </div>
 </div>
