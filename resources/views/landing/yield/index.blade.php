7fbe8dde72d4c9751808eb62da828bfe1a61e375
Itsqy <syatriarifqi@gmail.com>
2021-11-11 09:52:30 +0700

    first test

--- resources/views/landing/yield/index.blade.php
+++ resources/views/landing/yield/index.blade.php
@@ -2,50 +2,50 @@
 
 @section('content')
 
-{{-- biar carousel nya cuma ad pas di index doang , makanya dimasukin di index,blade ny aja , di panggil dsini --}}
+    {{-- biar carousel nya cuma ad pas di index doang , makanya dimasukin di index,blade ny aja , di panggil dsini --}}
 
-@include('landing.include.carousel')
+    @include('landing.include.carousel')
 
 
-<section class="container mt-5 mb-5">
-   {{-- PILIH LIGA  --}}
-   {{-- <section class="pilih-liga mt-4"> --}}
-    <h3><strong>Berdasarkan Kategori</strong></h3>
+    <section class="container mt-5 mb-5">
+        {{-- PILIH LIGA --}}
+        {{-- <section class="pilih-liga mt-4"> --}}
+        <h3><strong>Berdasarkan Kategori</strong></h3>
 
-    <div class="row mt-4">
-      @foreach($kategori as $k)
-       <div class="col">
-          <a style="text-decoration: none; color:black " href="{{route('landing.kategori', $k->slug)}}">
-             <div class="card shadow">
-                <div class="card-body text-center">
-{{$k->nama_kategori}}
+        <div class="row mt-4">
+            @foreach ($kategori as $k)
+                <div class="col">
+                    <a style="text-decoration: none; color:black " href="{{ route('landing.kategori', $k->slug) }}">
+                        <div class="card shadow">
+                            <div class="card-body text-center">
+                                {{ $k->nama_kategori }}
+                            </div>
+                        </div>
+                    </a>
                 </div>
-             </div>
-          </a>
-       </div>
-       @endforeach
-    </div>
- {{-- </section> --}}
-    <!-- card title -->
+            @endforeach
+        </div>
+        {{-- </section> --}}
+        <!-- card title -->
 
 
-    <div class="row mt-3 ">
-        @foreach ($produk as $pro)
-        <div class="col-md-3">
-            <div class="card"    >
-                <div class="card-body text-center">
-            <img src="{{url('storage/'.$pro->img)}}" class="img-fluid" alt="...">
-                    <div class="row mt-2">
-                        <h5 class="card-title">{{$pro->nama}}</h5>
-                        <p class="card-text">  Rp.{{number_format($pro->harga)}}</p>
-                        <a href="{{route('produk.detail',$pro->slug)}}" class="btn btn-primary">Order now! </a>
+        <div class="row mt-3 ">
+            @foreach ($produk as $pro)
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="{{ $pro->img }}" class="img-fluid" alt="...">
+                            <div class="row mt-2">
+                                <h5 class="card-title">{{ $pro->nama }}</h5>
+                                <p class="card-text"> Rp.{{ number_format($pro->harga) }}</p>
+                                <a href="{{ route('produk.detail', $pro->slug) }}" class="btn btn-primary">Order now! </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
 
 
-</section>
+    </section>
 @endsection
